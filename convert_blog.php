b4dbc332f00165d0c38b6374937cf2082d02c5ef
Masakatsu Nakamura <nakamura.masaskatsu@gmail.com>
2014-10-21 18:40:32 +0900

    変更中

--- convert_blog.php
+++ convert_blog.php
@@ -1,52 +1,38 @@
 <?php
+function outLines(&$array) {
+	echo implode(PHP_EOL, $message);
+	$array = [];
+}
+
 $in = fopen("php://stdin", "r");
 
 $in_body = false;
 $in_comment = false;
 $no_output = 0;
+$message = [];
 while (!feof($in)) {
 
 	$line = rtrim(fgets($in));
-	
-	if ($line == "AUTHOR:") {
-		
-		$message = [$line];
-		
-	} elseif ($line == "BODY:") {
 
+	if ($line == "BODY:" || $line == "EXTENDED BODY:") {
+		outLines($message);
 		$in_body = true;
-
+	} elseif (preg_match("/^CONVERT BREAKS: (.*)$/", $line, $match) == 1) {
+		$line_break = $match[1];
 	} elseif ($line == "COMMENT:") {
-
 		$in_comment = true;
-
-	} elseif ($in_body && $line == "EXTENDED BODY:") {
-
-		array_pop($message);
-		$no_output = 1;
-		
 	} elseif ($line == "-----") {
-
+		outLines($message);
 		$in_body = false;
 		$in_comment = false;
-
 	} elseif ($line == "--------") {
-
-		echo implode(PHP_EOL, $message);
-
+		outLines($message);
 	}
 
-	if ($in_body && substr($line, strlen($line) - 1, 1) != ">" && $line != "BODY:") {
-		$line .= "<br>";
-	}
-
-	while ($no_output == 0) {
-		if (!($in_comment && preg_match("/^(SECRET:|PASS:)/", $line))) {
-			if ($in_comment) {
-				$line = preg_replace("/^TITLE:/", "タイトル:");
-			}
-			array_push($message, $line);
+	if (!($in_comment && preg_match("/^(SECRET:|PASS:)/", $line))) {
+		if ($in_comment) {
+			$line = preg_replace("/^TITLE:/", $line, "タイトル:");
 		}
 	}
-	$no_output--;
+	array_push($message, $line);
 }
